d1a1a5ddb427844b6b4bcaa2197767510d5f4914
赵白杨 <zhaobaiyang@zhaobaiyangdeMacBook-Pro.local>
2017-03-08 10:47:40 +0800

    修剪多余代码

--- SimpleClock/SimpleClock/ViewController.swift
+++ SimpleClock/SimpleClock/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
+let BYRect = UIScreen.main.bounds
+let BYWidth = BYRect.size.width
+let BYHeight = BYRect.size.height
+
 class ViewController: UIViewController {
     
     var timer = Timer()
-    var countTime = 0
+    var countTime = 0.0
     let showTimeLabel = UILabel.init()
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,50 +31,51 @@ class ViewController: UIViewController {
     }
 
     func setupView() {
-        let pauseBtn = UIButton.init(type: .custom)
-        pauseBtn.frame = CGRect.init(x: self.view.frame.size.width/2, y: self.view.frame.size.height/2, width: self.view.frame.size.width/2, height: self.view.frame.size.height/2)
+        let pauseBtn = UIButton.init(type: UIButtonType.custom)
+        pauseBtn.frame = CGRect.init(x: BYWidth/2, y: BYHeight/3, width: BYWidth/2, height: BYHeight/3*2)
         
         pauseBtn.setTitle("暂停", for: .normal)
         pauseBtn.setTitle("继续", for: .selected)
         
         pauseBtn.isSelected = false
         
-        pauseBtn.backgroundColor = UIColor.blue
+        pauseBtn.backgroundColor = .green
         
         pauseBtn.addTarget(self, action: #selector(pauseAction(sender:)), for: .touchUpInside)
         
         self.view.addSubview(pauseBtn)
         
-        let endBtn = UIButton.init(type: .custom)
-        endBtn.frame = CGRect.init(x: 0, y: self.view.frame.size.height/2, width: self.view.frame.size.width/2, height: self.view.frame.size.height/2)
+        let endBtn = UIButton.init(type: UIButtonType.custom)
+        endBtn.frame = CGRect.init(x: 0, y: BYHeight/3, width: BYWidth/2, height: BYHeight/3*2)
         
         endBtn.setTitle("开始", for: .normal)
         endBtn.setTitle("结束", for: .selected)
         
         endBtn.isSelected = false
         
-        endBtn.backgroundColor = UIColor.orange
+        endBtn.backgroundColor = .blue
         
         endBtn.addTarget(self, action: #selector(startCountAction(sender:)), for: .touchUpInside)
         
         self.view.addSubview(endBtn)
         
-        showTimeLabel.frame = CGRect.init(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.height/2)
-        showTimeLabel.backgroundColor = UIColor.yellow
-        showTimeLabel.font = UIFont.boldSystemFont(ofSize: 200)
-        showTimeLabel.text = "0"
+        showTimeLabel.frame = CGRect.init(x: 0, y: 0, width: BYWidth, height: self.view.frame.height/3)
+        showTimeLabel.backgroundColor = UIColor.black
+        showTimeLabel.font = UIFont.systemFont(ofSize: 80)
+        showTimeLabel.text = "0.0"
         showTimeLabel.textColor = UIColor.white
         showTimeLabel.textAlignment = NSTextAlignment.center
         
         self.view.addSubview(showTimeLabel)
-        
+
     }
     
     func countUpSecond() {
         
-        countTime += 1
+        countTime += 0.1
         
-        showTimeLabel.text = "\(countTime)"
+//        showTimeLabel.text = "\(countTime)"
+        showTimeLabel.text = String(format: "%.2f", countTime)
 
     }
     
@@ -85,10 +94,10 @@ class ViewController: UIViewController {
     func startCountAction(sender: UIButton) {
         
         if !sender.isSelected {
-            self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countUpSecond), userInfo: nil, repeats: true)
+            self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(countUpSecond), userInfo: nil, repeats: true)
         } else {
             timer.invalidate()
-            showTimeLabel.text = "0"
+            showTimeLabel.text = "0.0"
             countTime = 0
         }
         
